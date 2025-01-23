e307711f377e03ee58eb6388cf651dc58bda99b0
sayful-hassan <sayf.fashion@gmail.com>
2025-01-23 11:29:46 +0530

    Git hub link is added

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -41,7 +41,7 @@ function Hero() {
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/sayful-hassan" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
           <a href="https://linkedin.com/" target="_blank">
