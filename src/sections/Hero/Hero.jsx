63f7a38eac7eed27385475b91fba3088d32d492b
sayful-hassan <sayf.fashion@gmail.com>
2025-01-25 13:11:03 +0530

    Resume link error fixed

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -9,7 +9,7 @@ import githubDark from '../../assets/github-dark.svg'
 import linkedinLight from '../../assets/linkedin-light.svg'
 import linkedinDark from '../../assets/linkedin-dark.svg'
 import { useTheme } from '../../common/ThemeContext'
-// import CV from '../../assets/cv.pdf'
+import '../../assets/saiful hassan.pdf'
 
 function Hero() {
     const { theme, toggleTheme } =useTheme();
