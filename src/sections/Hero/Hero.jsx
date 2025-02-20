722a342593d3c20f46fba93f72c02d3443f1a9b8
sayful-hassan <sayf.fashion@gmail.com>
2025-02-20 13:17:37 +0530

    Modified the linkedin link and logo to behance

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,8 +6,8 @@ import twitterLight from '../../assets/twitter-light.svg'
 import twitterDark from '../../assets/twitter-dark.svg'
 import githubLight from '../../assets/github-light.svg'
 import githubDark from '../../assets/github-dark.svg'
-import linkedinLight from '../../assets/linkedin-light.svg'
-import linkedinDark from '../../assets/linkedin-dark.svg'
+import behanceLight from '../../assets/Behance-light.svg'
+import behanceDark from '../../assets/Behance-dark.svg'
 import { useTheme } from '../../common/ThemeContext'
 import cv from '../../assets/saiful hassan CV.pdf'
 
@@ -17,7 +17,7 @@ function Hero() {
     const themeIcon = theme ==='light' ? sun : moon;
     const twitterIcon = theme ==='light' ? twitterLight : twitterDark;
     const githubIcon = theme ==='light' ? githubLight : githubDark;
-    const linkedinIcon = theme ==='light' ? linkedinLight : linkedinDark;
+    const behanceIcon = theme ==='light' ? behanceLight : behanceDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -44,8 +44,8 @@ function Hero() {
           <a href="https://github.com/sayful-hassan" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
-            <img src={linkedinIcon} alt="Linkedin icon" />
+          <a href="https://www.behance.net/saiful_hassan" target="_blank">
+            <img src={behanceIcon} alt="Behance icon" />
           </a>
         </span>
         <p className={styles.description}>
